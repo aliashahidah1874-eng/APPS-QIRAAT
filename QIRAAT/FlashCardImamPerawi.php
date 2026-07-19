2746748354a5ce734fcf0dbb0c3476b11b9056aa
aliashahidah1874-eng <aliashahidah1874@gmail.com>
2026-07-20 03:22:29 +0800

    Add files via upload

---
+++ QIRAAT/FlashCardImamPerawi.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Flashcard Imam & Perawi</title>
+
+<link rel="stylesheet" href="FlashCardImamPerawi.css">
+
+<link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+
+</head>
+
+<body>
+
+<div class="phone">
+
+    <!-- ================= HEADER ================= -->
+
+    <header class="header">
+
+        <a href="Homepage.php" class="back-btn">
+            <i class="fa-solid fa-arrow-left"></i>
+        </a>
+
+        <div>
+            <h2>بطاقة تعليمية</h2>
+            <p>بطاقات تعليمي</p>
+        </div>
+
+    </header>
+
+    <!-- ================= CONTENT ================= -->
+
+    <div class="content">
+
+        <div class="cards">
+
+<a href="Imamperawi.php?id=nafi" class="card">
+    <div class="card-content">
+        <h3>الامام نافع</h3>
+    </div>
+</a>
+
+<a href="Imamperawi.php?id=ibnkathir" class="card">
+    <div class="card-content">
+        <h3>الامام ابن كثير</h3>
+    </div>
+</a>
+
+<a href="Imamperawi.php?id=abuamr" class="card">
+    <div class="card-content">
+        <h3>الامام ابو عمرو</h3>
+    </div>
+</a>
+
+<a href="Imamperawi.php?id=ibnamir" class="card">
+    <div class="card-content">
+        <h3>الامام ابن عامر</h3>
+    </div>
+</a>
+
+<a href="Imamperawi.php?id=asim" class="card">
+    <div class="card-content">
+        <h3>الامام عاصم</h3>
+    </div>
+</a>
+
+<a href="Imamperawi.php?id=hamzah" class="card">
+    <div class="card-content">
+        <h3>الامام حمزة</h3>
+    </div>
+</a>
+
+<a href="Imamperawi.php?id=kisai" class="card">
+    <div class="card-content">
+        <h3>الامام الكيسائي</h3>
+    </div>
+</a>
+
+</div>
+
+    </div>
+
+    <!-- ================= BOTTOM NAVIGATION ================= -->
+
+    <nav class="bottom-nav">
+
+        <a href="Homepage.php">
+            <i class="fa-solid fa-house"></i>
+        </a>
+
+        <a href="FlashCardImamPerawi.php" class="active">
+            <i class="fa-solid fa-book-open"></i>
+        </a>
+
+        <a href="Audio.php">
+            <i class="fa-solid fa-headphones"></i>
+        </a>
+
+        <a href="Setting.php">
+            <i class="fa-solid fa-user"></i>
+        </a>
+
+    </nav>
+
+</div>
+
+</body>
+</html>
